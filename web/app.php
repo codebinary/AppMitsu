ae3452910382cadab3126a8da7a0e4861cb5b919
James Ontiveros <james.ontiveros86@gmail.com>
2017-03-31 17:27:14 -0500

    Mejorando las paginas de error

--- web/app.php
+++ web/app.php
@@ -12,6 +12,17 @@ $kernel = new AppKernel('prod', true);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
+
+//Configuraciones para los cors
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER["REQUEST_METHOD"];
+if($method == "OPTIONS"){
+    die();
+}
+
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
